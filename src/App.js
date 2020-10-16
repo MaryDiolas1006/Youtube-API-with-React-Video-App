db2f9c23c5554a59a28c32eac83dd6f1d3d8c5cf
Mary Ruth Diolas <mdiosgalla@gmail.com>
2020-10-16 14:25:29 +0800

    implemented async await syntax and state

--- src/App.js
+++ src/App.js
@@ -3,19 +3,23 @@ import SearchBar from './components/SearchBar';
 import youtubeApi from './components/youtubeApi';
 
 class App extends React.Component {
+  state = { videos: [] };
 
-  onSearchSubmit = text => {
-    youtubeApi.get('/search', {
+  onSearchSubmit = async text => {
+    const response = await youtubeApi.get('/search', {
       params: {
         q: text
       }
     })
+    this.setState({ videos: response.data.items }); 
   }
 
   render() {
     return (
       <div className= "ui container">
         <SearchBar onTextSubmit={this.onSearchSubmit} />
+
+        I have {this.state.videos.length} videos
       </div>
     )
   }
